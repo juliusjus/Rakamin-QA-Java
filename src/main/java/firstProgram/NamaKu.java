2763fa3cc3b4e13d18c62dd599a273d3d143cab3
juliusjus <j.y.hartono@gmail.com>
2023-10-20 22:57:47 +0700

    new println

--- src/main/java/firstProgram/NamaKu.java
+++ src/main/java/firstProgram/NamaKu.java
@@ -3,5 +3,7 @@ package firstProgram;
 public class NamaKu {
     public static void main(String[] args) {
         System.out.println("Namaku Yusdianto");
+        System.out.println("Beda commit");
+
     }
 }
